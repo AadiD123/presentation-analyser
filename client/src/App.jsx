26db32ac222961b13bf4dc929190948459aa3fb1
arm-mahesh <armanyamaheshwari@gmail.com>
2024-01-20 13:37:08 -0500

    authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import WebcamVideo from "./components/Webcam";
+import LoginButton from "./components/LoginButton";
 
 function App() {
   return (
     <div>
+      <LoginButton />
       <WebcamVideo />
     </div>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain="dev-l3swl5d3gwfqxc3c.us.auth0.com"
+    clientId="t0ITlhfVtRYFhEYQ3sxg3d5tPiuIoGRg"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+      <App />
+    </Auth0Provider>
+    
   </React.StrictMode>,
 )
