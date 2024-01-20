b0475611e7b66fb0d1b66ae7e4aae2048b316188
Aaditya Doiphode <aadityad27@gmail.com>
2024-01-20 13:03:20 -0500

    webcam

--- client/src/components/Webcam.jsx
+++ client/src/components/Webcam.jsx
@@ -74,47 +74,46 @@ export default function WebcamVideo() {
     mediaRecorderRef.current.stop();
     setCapturing(false);
     if (recordedChunks.length) {
-      console.log("Chunks before sending:", recordedChunks); // Add this line
       sendVideoToServer(recordedChunks);
-    } else {
-      console.log("No chunks to send"); // Add this line
     }
   }, [mediaRecorderRef, setCapturing, recordedChunks]);
 
-  // const handleDownload = useCallback(() => {
-  //   if (recordedChunks.length) {
-  //     const blob = new Blob(recordedChunks, {
-  //       type: "video/webm",
-  //     });
-  //     const url = URL.createObjectURL(blob);
-  //     const a = document.createElement("a");
-  //     document.body.appendChild(a);
-  //     a.style = "display: none";
-  //     a.href = url;
-  //     a.download = "react-webcam-stream-capture.webm";
-  //     a.click();
-  //     window.URL.revokeObjectURL(url);
-  //     setRecordedChunks([]);
-  //   }
-  // }, [recordedChunks]);
-
-  const sendVideoToServer = async (videoChunks) => {
-    const blob = new Blob(videoChunks, { type: "video/webm" });
-    console.log("Sending video to server", blob);
-
-    let formData = new FormData();
-    formData.append("video", videoBlob, "videoFileName.mp4");
-
-    try {
-      console.log("sending");
-      await fetch("http://localhost:3000/api/video", {
-        method: "POST",
-        body: blob,
+  const handleDownload = useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
       });
-    } catch (error) {
-      console.error("Error sending video to server:", error);
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
     }
-  };
+  }, [recordedChunks]);
+
+  // const sendVideoToServer = async (videoChunks) => {
+  //   const blob = new Blob(videoChunks, { type: "video/webm" });
+  //   console.log("Sending video to server", blob);
+
+  //   var formData = new FormData();
+
+  //   // Append the Blob with correct type and filename
+  //   formData.append("video", blob, "video.webm");
+
+  //   try {
+  //     console.log("sending");
+  //     await fetch("http://localhost:3000/upload-video", {
+  //       method: "POST",
+  //       body: formData,
+  //     });
+  //   } catch (error) {
+  //     console.error("Error sending video to server:", error);
+  //   }
+  // };
 
 
 
@@ -125,7 +124,8 @@ export default function WebcamVideo() {
   return (
     <div>
       <Webcam
-        audio={false}
+        audio={true}
+        muted={true}
         mirrored={true}
         ref={webcamRef}
         videoConstraints={videoConstraints}
@@ -144,11 +144,11 @@ export default function WebcamVideo() {
 
           </>
         )}
-        {/* {recordedChunks.length > 0 && (
+        {recordedChunks.length > 0 && (
           <button className="btn ml-2" onClick={handleDownload}>
             Download
           </button>
-        )} */}
+        )}
       </div>
       <div>
             {maxEmotions.map(e => (<div key={e.name}>
