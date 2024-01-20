6f09f752cf124ef65de174b0575aa0d285676351
Arnav Kulkarni <arnavyk@gmail.com>
2024-01-20 16:39:38 -0500

    remove dysfunctional import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import WebcamVideo from "./components/Webcam";
 import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
+// import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
 
 function App() {
