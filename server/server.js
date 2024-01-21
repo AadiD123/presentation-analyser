b6bcceef779db746e24f0f9f028e0687fd706cc2
arm-mahesh <armanyamaheshwari@gmail.com>
2024-01-20 19:08:18 -0500

    yolo

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const { exec } = require("child_process");
 const multer = require("multer");
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const uploadsDir = './uploads';
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -21,8 +27,25 @@ app.post('/upload-video', upload.single('video'), async (req, res) => {
     const timestamp = Date.now();
     const videoFileName = `video_${timestamp}.webm`;
     const videoFilePath = `./uploads/${videoFileName}`;
-    require('fs').writeFileSync(videoFilePath, videoBlob);
-    res.status(200).send('Video uploaded successfully');
+    fs.writeFileSync(videoFilePath, videoBlob);
+    const wavFileName = `audio_${timestamp}.wav`;
+    const wavFilePath = `./uploads/${wavFileName}`;
+
+    ffmpeg()
+      .input(videoFilePath)
+      .audioCodec('pcm_s16le')
+      .audioChannels(1)
+      .audioBitrate(16)
+      .audioFrequency(16000)
+      .on('end', () => {
+        console.log('Audio conversion finished');
+        res.status(200).send('Video and audio uploaded successfully');
+      })
+      .on('error', (err) => {
+        console.error('Error converting audio:', err);
+        res.status(500).send('Error converting audio');
+      })
+      .save(wavFilePath);
   } catch (error) {
     console.error('Error uploading video:', error);
     res.status(500).send('Error uploading video');
