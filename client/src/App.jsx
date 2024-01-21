d1d918e3810d9b7e4f1b73c7c109f9f88a621767
Arnav Kulkarni <arnavyk@gmail.com>
2024-01-20 21:34:41 -0500

    stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar";
 import WebcamVideo from "./components/Webcam";
 
 function App() {
-  return (
+  return (<>
     <div className="dotted-background">
       <Navbar />
       <WebcamVideo />
     </div>
+    <script src="webgazer.js" type="text/javascript" />
+      </>
   );
 }
 
