9ab809d8fff7d26abf04f659dbd431816d082db6
Arnav Kulkarni <arnavyk@gmail.com>
2024-01-21 05:38:51 -0500

    add auth0 stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import VideoPlayer from "./components/VideoPlayer";
 import WebcamVideo from "./components/Webcam";
+import { useEffect } from "react";
+import axios from "axios"
 
 function App() {
   const videoSrc = "./src/assets/T-Pain.mp4";
+  const {isAuthenticated, user, error, isLoading} = useAuth0()
+  useEffect(() => {
+    console.log("user", user)
+    if (user) {
+      // send post req
+       axios
+        .post(`http://localhost:3000/login/${user.email}`)
+        .then((res) => {
+          console.log(res);
+        });
+    }
+  }, [user])
   return (
     <>
       <div className="dotted-background">
